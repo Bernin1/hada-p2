97482f26c9444ee17f28ff9d888fca08d4278459
Denis <dgv26@alu.ua.es>
2026-02-17 17:39:26 +0100

    30840640D añadido eventos.cs

---
+++ hada-p2/Eventos.cs
@@ -0,0 +1,31 @@
+﻿using Hada;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+{
+    public class TocadoArgs
+    {
+        public string Nombre { get; }
+        public Coordenada CoordenadaImpacto { get; }
+
+        public TocadoArgs(string nombre, Coordenada coordenadaImpacto)
+        {
+            Nombre = nombre;
+            CoordenadaImpacto = coordenadaImpacto;
+        }
+    }
+
+    public class HundidoArgs
+    {
+        public string Nombre { get; }
+
+        public HundidoArgs(string nombre)
+        {
+            Nombre = nombre;
+        }
+    }
+}
