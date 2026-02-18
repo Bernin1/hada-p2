c28e4d7e7a8fdc68f60c08deb090f114a380aeca
Denis <dgv26@alu.ua.es>
2026-02-18 16:33:13 +0100

    30840640D retocado Eventos.cs

---
+++ hada-p2/Eventos.cs
@@ -0,0 +1,33 @@
+﻿using Hada;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+{
+    //Evento cuando un barco es tocado
+    public class TocadoArgs
+    {
+        public string Nombre { get; }
+        public Coordenada CoordenadaImpacto { get; }
+
+        public TocadoArgs(string nombre, Coordenada coordenadaImpacto)
+        {
+            Nombre = nombre;
+            CoordenadaImpacto = coordenadaImpacto;
+        }
+    }
+
+    //Evento cuando un barco es hundido
+    public class HundidoArgs
+    {
+        public string Nombre { get; }
+
+        public HundidoArgs(string nombre)
+        {
+            Nombre = nombre;
+        }
+    }
+}
